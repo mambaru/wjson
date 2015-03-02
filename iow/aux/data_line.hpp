3192772b3a65ec71b9e59d72b0d155aa4dcb2dfd
Vladimir Migashko <migashko@gmail.com>
2015-03-03 06:27:49 +0800

    upd

--- iow/aux/data_line.hpp
+++ iow/aux/data_line.hpp
@@ -6,17 +6,43 @@
 #include <boost/concept_check.hpp>
 
 namespace iow{ 
- 
+
+enum struct data_line_split_mode{
+  disable,        // [default] запретить разбиение пакетов
+  enable,         // Разбивать все пакеты, которые большe buffsize
+  except_first,   // Исключить первый, но разбить остаток при подтверждении
+  except_confirm  // Исключить первый и не разбивать остаток при подтверждении
+};
+
+enum struct data_line_merge_mode{
+  // Объединение происходит только если линия не пуста
+  disable,        // [default] запретить объединение пакетов
+  enable,         // Объеденять со следующим, если суммарный размер не превышает buffsize
+  except_confirm  // Не объединять со следующим при подтверждении
+};
+
+enum struct data_line_mode{
+
+};
+
 struct data_line_options
 {
-  /*
-  size_t bufsize = 8192;        // 8KB, 0 - without buffer splitting
-  size_t wrnsize = 0;           // 1MB
-  size_t maxsize = 0;           // 1GB
-  */
-  size_t bufsize;        // 8KB, 0 - without buffer splitting
+  typedef data_line_split_mode split_mode;
+  typedef data_line_merge_mode merge_mode;
+  typedef data_line_mode mode;
+  ;
+  size_t bufsize;           // 8KB, 0 - without buffer splitting
   size_t wrnsize;           // 1MB
   size_t maxsize;           // 1GB
+  //split_mode split;
+  //merge_mode merge;
+  //bool strong_mode; //?? default=false, при false допустимо двухкратное превышение буфера 
+  
+  size_t maxbuff; // Если меньше bufsize, то == bufsize
+  size_t minbuff; // Если больше bufsize, то == bufsize
+  // TODO: дурацкие название
+  bool except_first; // Если maxbuff или minbuff != 0 и bufsize!=0
+  bool except_confirm;
 };
 
 template<typename DataType>
@@ -52,7 +78,6 @@ public:
     if ( d == nullptr )
     {
       // Взять следующую порцию (если есть)
-      
       if ( !_line.empty() && _line.front()==nullptr )
       {
         // Это подверждение отправки
@@ -92,13 +117,29 @@ public:
       {
         _line.push( std::move(d) );
       }
-      
     }
     return nullptr;
   }
-  
+
+
+  data_ptr confirm(data_ptr d, size_t size_confirmed)
+  {
+    if ( _line.empty() )
+      return nullptr; // TODO exception
+    if ( _line.front()!=nullptr )
+      return nullptr; // TODO exception
+    // TODO: split or merge if 
+    return nullptr;
+  }
+
+
   void rollback(data_ptr d)
   {
+    if ( _line.empty() )
+      return; // TODO exception
+    if ( _line.front()!=nullptr )
+      return; // TODO exception
+    // TODO: split or merge if 
     _line.front() = std::move(d);
   }
   
--- tests/aux/data_test.cpp
+++ tests/aux/data_test.cpp
@@ -7,6 +7,14 @@ struct _data_line_;
 struct _generator_;
 struct _data_size_;
 struct _data_line_init_;
+struct _test_options_;
+
+struct data_line_test_options
+{
+  ::iow::data_line_options dlo;
+  size_t data_size;
+  size_t data_count;
+};
 
 struct generator
 {
@@ -24,93 +32,74 @@ struct generator
     //d->push_back('A');
     return std::move(d);
   }
-
 };
 
-template<
-  size_t DataSize, 
-  size_t DataCount, 
-  size_t BufSize, 
-  size_t WrnSize,  
-  size_t MaxSize
->
-struct data_line_init
-  : fas::testing::unit
+UNIT(data_line_init, "")
 {
-public:
-  
-  data_line_init() 
-    : fas::testing::unit("data_line_init", "data_line_init")
-  {}
-  
-  template<typename T>
-  void operator()(T& t)
+  auto& data_line = t.get_aspect().template get<_data_line_>();
+  typedef typename std::remove_reference<decltype(data_line)>::type data_line_t;
+  //typedef typename data_line_t::data_type data_type;
+  typedef typename data_line_t::data_ptr data_ptr;
+
+  auto& opt = t.get_aspect().template get<_test_options_>();
+
+  data_line.clear();
+  data_line.set_options(opt.dlo);
+
+  data_ptr first;
+  for (size_t i =0; i < opt.data_count; i++)
   {
-    typedef typename T::aspect::template advice_cast<_data_line_>::type data_line_t;
-    typedef typename data_line_t::data_type data_type;
-    typedef typename data_line_t::data_ptr data_ptr;
-    
-    data_line_t& data_line = t.get_aspect().template get<_data_line_>();
-    
-    data_line.clear();
-    data_line.set_options({BufSize, WrnSize, MaxSize});
-    
-    data_ptr first;
-    for (size_t i =0; i < DataCount; i++)
-    {
-      data_ptr d = std::make_unique<data_type>();
-      for (size_t j =0; j < DataCount; j++)
-        d->push_back('A'+j%10);
-      if (i==0)
-        first = data_line.create(std::move(d));
-      else
-        data_line.create(std::move(d));
-    }
-    data_line.rollback( std::move(first) );
-  }
-};
-
-
+    data_ptr d = t.get_aspect().template get<_generator_>()(t, i, opt.data_size);
+    if (i==0)
+      first = data_line.create(std::move(d));
+    else
+      data_line.create(std::move(d));
+   }
+   data_line.rollback( std::move(first) );
+}
 
 UNIT(data_line_first, "")
 {
   using namespace fas::testing;
   typedef typename T::aspect::template advice_cast<_data_line_>::type data_line_t;
-  typedef typename data_line_t::data_type data_type;
-  typedef typename data_type::value_type value_type;
-  typedef typename data_line_t::data_ptr data_ptr;
   data_line_t& data_line = t.get_aspect().template get<_data_line_>();
+  auto& opt = t.get_aspect().template get<_test_options_>();
+
   size_t counter = 0;
   auto d = data_line.create(nullptr);
   while (d!=nullptr)
   {
     auto dd = t.get_aspect().template get<_generator_>()(t, counter, d->size());
-    counter += d->size();
-    t << equal<assert>(*dd, *d) << *dd << "!=" << *d << FAS_TESTING_FILE_LINE;
+    counter ++;
+    t << equal<assert>(d->size(), opt.dlo.bufsize) << d->size() << "!=" << opt.dlo.bufsize <<": "<< FAS_TESTING_FILE_LINE;
+    t << equal<assert>(*dd, *d) << *dd << "!=" << *d <<": "<< FAS_TESTING_FILE_LINE;
+    std::cout<< std::endl<< *dd << "==" << *d << std::endl;
     d = data_line.create(nullptr);
   }
 }
 
-template<size_t DataSize, size_t DataCount, size_t BufSize, size_t WrnSize,  size_t MaxSize>
-BEGIN_UNIT_LIST(data_line)
-  , fas::advice<_data_line_init_, data_line_init<DataSize, DataCount, BufSize, WrnSize, MaxSize> >
-  , fas::group< fas::testing::_units_, _data_line_init_>
+
+BEGIN_SUITE(aux,"aux suite")
+  ADD_UNIT(data_line_init)
   ADD_UNIT(data_line_first)
-END_UNIT_LIST(data_line)
-
-
-BEGIN_SUITE(aux,"")
-  ADD_UNIT_LIST(data_line)<10, 10,  10, 10, 10>
+  ADD_VALUE(_test_options_, data_line_test_options )
   ADD_VALUE(_data_line_, ::iow::data_line< std::string > )
   ADD_ADVICE(_generator_, generator)
 END_SUITE(aux)
 
 ::fas::testing::suite_counts my_suite_run(int , char*[])
 {
+  data_line_test_options optlist[]={
+    { {3,10,10}, 10,10},
+    { {10,10,10}, 10,10}
+  };
+  std::cout << sizeof(optlist) <<std::endl;
   ::fas::testing::suite_counts sc;
   aux_suite as;
-  for (int i =0 ; i < 10; i++)
+  auto& opt = as.get_aspect().get<_test_options_>();
+  for (size_t i =0 ; i < std::extent<decltype(optlist)>::value; i++)
   {
+    opt=optlist[i];
     as.run();
     sc += as.counts();
   }
@@ -119,6 +108,6 @@ END_SUITE(aux)
 
 
 BEGIN_TEST
-  RUN_SUITE(aux)
+  //RUN_SUITE(aux)
   RUN_SUITE(my)
 END_TEST
