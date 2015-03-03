3c01883791fb30a02b7e26638234049fc0a78b2a
Vladimir Migashko <migashko@gmail.com>
2015-03-03 20:49:29 +0300

    data_line wrk

--- iow/aux/data_line.hpp
+++ iow/aux/data_line.hpp
@@ -1,12 +1,14 @@
 #pragma once
 
 #include <iow/aux/memory.hpp>
-#include <queue>
+#include <list>
 #include <memory>
+#include <stdexcept>
 #include <boost/concept_check.hpp>
 
 namespace iow{ 
 
+  /*
 enum struct data_line_split_mode{
   disable,        // [default] запретить разбиение пакетов
   enable,         // Разбивать все пакеты, которые большe buffsize
@@ -24,25 +26,31 @@ enum struct data_line_merge_mode{
 enum struct data_line_mode{
 
 };
+*/
+
+
+template<typename N>
+class data_line_excepttion
+  : public std::logic_error
+{
+public:
+  data_line_excepttion()
+    : std::logic_error( N()() )
+  {
+  }
+};
 
 struct data_line_options
 {
-  typedef data_line_split_mode split_mode;
-  typedef data_line_merge_mode merge_mode;
-  typedef data_line_mode mode;
-  ;
-  size_t bufsize;           // 8KB, 0 - without buffer splitting
-  size_t wrnsize;           // 1MB
-  size_t maxsize;           // 1GB
-  //split_mode split;
-  //merge_mode merge;
-  //bool strong_mode; //?? default=false, при false допустимо двухкратное превышение буфера 
-  
-  size_t maxbuff; // Если меньше bufsize, то == bufsize
-  size_t minbuff; // Если больше bufsize, то == bufsize
+  size_t bufsize /*= 8*1024*/;
+  size_t maxbuf  /*= 8*1024*/; 
+  size_t minbuf  /*= 128*/; 
+  size_t wrnsize /*= 1024*1024*/;
+  size_t maxsize /*= 1024*1024*1024*/;
+
   // TODO: дурацкие название
-  bool except_first; // Если maxbuff или minbuff != 0 и bufsize!=0
-  bool except_confirm;
+  bool except_first /*= true*/; // Если maxbuff или minbuff != 0 и bufsize!=0
+  bool except_confirm /*= true*/;
 };
 
 template<typename DataType>
@@ -52,28 +60,249 @@ public:
   typedef data_line_options options_type;
   typedef DataType data_type;
   typedef std::unique_ptr<data_type> data_ptr;
+  typedef typename data_type::iterator iterator;
   
   data_line()
-    : _options({8192,0,0})
+    : _options()
     , _size(0)
-  {}
+  {
+  }
   
   void set_options(const options_type& options) 
   {
     _options = options;
+    
+    if ( _options.bufsize == 0 )
+    {
+      _options.bufsize = 1024 * 8;
+    }
+    
+    if ( _options.minbuf > _options.bufsize )
+    {
+      _options.minbuf = _options.bufsize;
+    }
+    
+    if ( _options.maxbuf < _options.bufsize )
+    {
+      _options.maxbuf = _options.bufsize;
+    }
   }
   
   const options_type& options() const
   {
     return _options;
   }
+
+  data_ptr next(data_ptr d)
+  {
+    if ( _line.empty() )
+      return this->next_first_(std::move(d));
+    else
+      return this->next_rest_(std::move(d));
+  }
+
+  // Если данные отправленны все 
+  data_ptr confirm_next()
+  {
+    this->check_confirm_();
+    _line.pop_front();
+    if ( _line.empty() )
+      return nullptr;
+    _size -= _line.front()->size();
+    return std::move( _line.front() );
+  }
   
+  data_ptr confirm_next(data_ptr d, size_t size_confirmed)
+  {
+    if ( d->size() == size_confirmed )
+      return this->confirm_next();
+    
+    if (d->size() < size_confirmed )
+      throw; // TODO exception
+      
+    size_t tailsize = d->size() - size_confirmed;
+    std::copy( d->begin() + size_confirmed, d->end(), d->begin() ); 
+    d->resize(tailsize);
+    
+    size_t cursize = d->size();
+    if ( !_options.except_confirm && cursize < _options.minbuf && _line.size() > 1 )
+    {
+      auto itr = _line.begin() + 1;
+      size_t nextsize = (*itr)->size();
+      if ( cursize + nextsize < _options.maxbuf )
+      {
+        std::copy( (*itr)->begin(), (*itr)->end(), std::inserter( *d, d->end()));
+        _size -= (*itr)->size();
+        _line.pop_front();
+        _line.back() = nullptr;
+      }
+    }
+  }
+
+  void rollback(data_ptr d)
+  {
+    if ( _line.empty() )
+      return; // TODO exception
+    if ( _line.front()!=nullptr )
+      return; // TODO exception
+    _line.front() = std::move(d);
+    if (d!=nullptr)
+      _size += d->size();
+  }
+
+  void push_back(data_ptr d)
+  {
+    if ( d == nullptr )
+      return;
+    size_t offset = this->merge_(d->begin(), d->end());
+    this->push_back_( std::move(d), offset );
+  }
+
+  
+  size_t size() const 
+  {
+    return _size;
+  }
+  
+  bool is_full() const
+  {
+    return _options.maxsize!=0 && _size > _options.maxsize;
+  }
+  
+  bool is_wrn() const
+  {
+    return _options.wrnsize!=0 && _size > _options.wrnsize;
+  }
+  
+  void clear()
+  {
+    _line.clear();
+    _size = 0;
+  }
+
+private:
+  
+  data_ptr next_first_(data_ptr d)
+  {
+    if ( d == nullptr )
+      return nullptr;
+
+    if ( _options.except_first )
+      return std::move(d);
+    
+    if ( d->size() <  _options.maxbuf )
+      return std::move(d);
+    
+    this->push_back_( d->begin() + _options.bufsize, d->end() );
+    d->resize(_options.bufsize);
+    return std::move(d);
+  }
+
+  data_ptr next_rest_(data_ptr d)
+  {
+    if ( d!=nullptr )
+    {
+      this->push_back( std::move(d) );
+    }
+    
+    if ( _line.front() == nullptr )
+    {
+      return nullptr;
+    }
+    
+    d = std::move( _line.front() );
+    _line.pop_front();
+    _size -= d->size();
+    return std::move(d);
+  }
+
+  
+  void check_confirm_()
+  {
+    if ( _line.empty() )
+      throw ; // TODO exception
+    if ( _line.front()!=nullptr )
+      throw ; // TODO exception
+  }
+  
+  void push_back_(iterator beg, iterator end)
+  {
+    auto cur = beg; 
+    while (beg!=end)
+    {
+      cur = std::distance(beg,end) < static_cast<std::ptrdiff_t>(_options.maxbuf)
+            ? end
+            : beg + _options.bufsize;
+        
+      data_ptr d = std::make_unique<data_type>(beg, cur);
+      _size += d->size();
+      _line.push_back(std::move(d));
+      beg = cur;
+    }
+  }
+  
+  void push_back_(data_ptr d, size_t offset)
+  {
+    _line.push_back( std::move(d) );
+    auto& dd = _line.back();
+    bool more = std::distance( dd->begin() + offset, dd->end() ) > static_cast<std::ptrdiff_t>(_options.maxbuf);
+    if ( more )
+    {
+      size_t tailpos = offset + _options.bufsize;
+      this->push_back_(dd->begin() + tailpos, dd->end() );
+      std::copy( dd->begin() + offset, dd->begin() + tailpos, dd->begin() );
+      dd->resize(_options.bufsize);
+    }
+    else
+    {
+      std::copy( dd->begin() + offset, dd->end(), dd->begin() );
+      dd->resize(dd->size() - offset );
+    }
+    _size += dd->size();
+  }
+  
+  // Объеденить с хвостом (если имеет смысл)
+  size_t merge_( iterator beg, iterator end )
+  {
+    size_t size=0;
+    // Объединить с последним элементом?
+    bool merge = !_line.empty() && _line.back()!=nullptr && _line.back()->size() < _options.minbuf;
+    
+    if ( merge )
+    {
+      auto& d = _line.back();
+      size_t backsize = d->size();
+      size_t cursize = std::distance(beg, end);
+      
+      // Сколько скопировать в хвост
+      size_t size = _options.bufsize - backsize;
+      
+      bool fulmerge = ( cursize <= size )
+                        || ( cursize - size < _options.minbuf 
+                             && backsize + cursize < _options.maxbuf );
+                        
+      if ( fulmerge )
+      {
+        size = cursize;
+      }
+      d->reserve(d->size() + size);
+      std::copy(
+        beg, 
+        beg + size,
+        std::inserter( *d, d->end() )
+      );
+      _size += size;
+    }
+    return size;
+  }
+
+private:
   /**
    * @param d - данные или nullptr
    * @result следующая порция данных
    * 
    */
-  data_ptr create(data_ptr d)
+  data_ptr create_old(data_ptr d)
   {
     if ( d == nullptr )
     {
@@ -121,56 +350,11 @@ public:
     return nullptr;
   }
 
-
-  data_ptr confirm(data_ptr d, size_t size_confirmed)
-  {
-    if ( _line.empty() )
-      return nullptr; // TODO exception
-    if ( _line.front()!=nullptr )
-      return nullptr; // TODO exception
-    // TODO: split or merge if 
-    return nullptr;
-  }
-
-
-  void rollback(data_ptr d)
-  {
-    if ( _line.empty() )
-      return; // TODO exception
-    if ( _line.front()!=nullptr )
-      return; // TODO exception
-    // TODO: split or merge if 
-    _line.front() = std::move(d);
-  }
-  
-  size_t size() const 
-  {
-    return _size;
-  }
-  
-  bool is_full() const
-  {
-    return _options.maxsize!=0 && _size > _options.maxsize;
-  }
-  
-  bool is_wrn() const
-  {
-    return _options.wrnsize!=0 && _size > _options.wrnsize;
-  }
-  
-  void clear()
-  {
-    while ( !_line.empty() )
-    {
-      _line.pop();
-    }
-    _size = 0;
-  }
   
 private:
   options_type _options;
   size_t _size;
-  std::queue<data_ptr> _line;
+  std::list<data_ptr> _line;
 };
 
 }
--- tests/aux/data_test.cpp
+++ tests/aux/data_test.cpp
@@ -5,16 +5,43 @@
 
 struct _data_line_;
 struct _generator_;
+struct _init_line_;
+
 struct _data_size_;
 struct _data_line_init_;
 struct _test_options_;
 
-struct data_line_test_options
+struct data_line_test_options: iow::data_line_options
 {
-  ::iow::data_line_options dlo;
   size_t data_size;
   size_t data_count;
 };
+
+data_line_test_options getopt(
+  size_t bufsize,
+  size_t maxbuf, 
+  size_t minbuf, 
+  size_t wrnsize,
+  size_t maxsize,
+  bool except_first,
+  bool except_confirm,
+  size_t data_size,
+  size_t data_count
+)
+{
+  data_line_test_options dlto;
+  dlto.bufsize = bufsize;
+  dlto.maxbuf = maxbuf; 
+  dlto.minbuf = minbuf;
+  dlto.wrnsize = wrnsize;
+  dlto.maxsize = maxsize;
+  dlto.except_first = except_first;
+  dlto.except_confirm = except_confirm;
+  dlto.data_size = data_size;
+  dlto.data_count = data_count;
+  return dlto;
+}
+
 
 struct generator
 {
@@ -34,64 +61,74 @@ struct generator
   }
 };
 
-UNIT(data_line_init, "")
+//UNIT(data_line_init, "")
+struct init_line
 {
-  auto& data_line = t.get_aspect().template get<_data_line_>();
-  typedef typename std::remove_reference<decltype(data_line)>::type data_line_t;
-  //typedef typename data_line_t::data_type data_type;
-  typedef typename data_line_t::data_ptr data_ptr;
-
-  auto& opt = t.get_aspect().template get<_test_options_>();
-
-  data_line.clear();
-  data_line.set_options(opt.dlo);
-
-  data_ptr first;
-  for (size_t i =0; i < opt.data_count; i++)
+  template<typename T>
+  void operator()(T& t)
   {
-    data_ptr d = t.get_aspect().template get<_generator_>()(t, i, opt.data_size);
-    if (i==0)
-      first = data_line.create(std::move(d));
-    else
-      data_line.create(std::move(d));
-   }
-   data_line.rollback( std::move(first) );
-}
+    auto& data_line = t.get_aspect().template get<_data_line_>();
+    typedef typename std::remove_reference<decltype(data_line)>::type data_line_t;
+    //typedef typename data_line_t::data_type data_type;
+    typedef typename data_line_t::data_ptr data_ptr;
+
+    auto& opt = t.get_aspect().template get<_test_options_>();
+
+    data_line.clear();
+    data_line.set_options(opt);
+    static_cast<iow::data_line_options&>(opt) = data_line.options();
+
+    data_ptr first;
+    for (size_t i =0; i < opt.data_count; i++)
+    {
+      data_ptr d = t.get_aspect().template get<_generator_>()(t, i, opt.data_size);
+      data_line.push_back(std::move(d));
+      /*if (i==0)
+        first = data_line.next(std::move(d));
+      else
+        data_line.next(std::move(d));
+      */
+    }
+    //data_line.rollback( std::move(first) );
+  }
+};
 
 UNIT(data_line_first, "")
 {
   using namespace fas::testing;
+  
   typedef typename T::aspect::template advice_cast<_data_line_>::type data_line_t;
+  t.get_aspect().template get<_init_line_>()(t);
   data_line_t& data_line = t.get_aspect().template get<_data_line_>();
   auto& opt = t.get_aspect().template get<_test_options_>();
-
   size_t counter = 0;
-  auto d = data_line.create(nullptr);
+  auto d = data_line.next(nullptr);
   while (d!=nullptr)
   {
     auto dd = t.get_aspect().template get<_generator_>()(t, counter, d->size());
     counter ++;
-    t << equal<assert>(d->size(), opt.dlo.bufsize) << d->size() << "!=" << opt.dlo.bufsize <<": "<< FAS_TESTING_FILE_LINE;
+    t << equal<assert>(d->size(), opt.bufsize) << d->size() << "!=" << opt.bufsize <<": "<< FAS_TESTING_FILE_LINE;
     t << equal<assert>(*dd, *d) << *dd << "!=" << *d <<": "<< FAS_TESTING_FILE_LINE;
     std::cout<< std::endl<< *dd << "==" << *d << std::endl;
-    d = data_line.create(nullptr);
+    d = data_line.next(nullptr);
   }
 }
 
 
 BEGIN_SUITE(aux,"aux suite")
-  ADD_UNIT(data_line_init)
   ADD_UNIT(data_line_first)
   ADD_VALUE(_test_options_, data_line_test_options )
   ADD_VALUE(_data_line_, ::iow::data_line< std::string > )
   ADD_ADVICE(_generator_, generator)
+  ADD_ADVICE(_init_line_, init_line)
 END_SUITE(aux)
 
 ::fas::testing::suite_counts my_suite_run(int , char*[])
 {
   data_line_test_options optlist[]={
-    { {3,10,10}, 10,10},
-    { {10,10,10}, 10,10}
+  //       bufsize maxbuf minbuf wrnsize maxsize except_first except_confirm data_size data_count
+    getopt(0,      0,     0,     0,      0,      true,        true,          10,       10),
+    getopt(0,      0,     0,     0,      0,      true,        true,          10,       10)
   };
   std::cout << sizeof(optlist) <<std::endl;
   ::fas::testing::suite_counts sc;
