763f95f41f308ba8c721bd244b63b75f6027f69f
Vladimir Migashko <migashko@gmail.com>
2016-04-01 20:05:28 +0300

    upd

--- tests/workflow/workflow_test.cpp
+++ tests/workflow/workflow_test.cpp
@@ -1,9 +1,10 @@
 #include <fas/testing.hpp>
 
 BEGIN_TEST
-  RUN_SUITE(delayed_queue)
+  /*RUN_SUITE(delayed_queue)
   RUN_SUITE(timed_queue)
   RUN_SUITE(timer_manager)
   RUN_SUITE(thread_pool)
-  RUN_SUITE(workflow)
+  RUN_SUITE(workflow)*/
+  RUN_SUITE(rrqueue)
 END_TEST
