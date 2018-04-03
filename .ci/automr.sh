fcaf618dea9923753cd49c9bc25e01565802caa3
Vladimir Migashko <migashko@gmail.com>
2018-04-04 01:50:39 +0300

    *ci

--- .ci/automr.sh
+++ .ci/automr.sh
@@ -3,10 +3,7 @@
 # Extract the host where the server is running, and add the URL to the APIs
 
 remove_source_branch=false
-
-[[ $HOST =~ ^https?://[^/]+ ]] && HOST="${BASH_REMATCH[0]}/api/v4/projects/"
-
-TARGET_BRANCH=`curl --silent "${HOST}${CI_PROJECT_ID}" --header "PRIVATE-TOKEN:${PRIVATE_TOKEN}" | python3 -c "import sys, json; print(json.load(sys.stdin)['default_branch'])"`;
+TARGET_BRANCH="master"
 
 # The description of our new MR, we want to remove the branch after the MR has
 # been closed
