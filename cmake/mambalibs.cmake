9447ae996ee5c2866f64bb4153de5607270376e9
Vladimir Migashko <migashko@gmail.com>
2018-06-29 16:44:58 +0300

    *cmake

---
+++ cmake/mambalibs.cmake
@@ -0,0 +1,65 @@
+# ConfigureLibrary.cmake
+#
+# Configure library
+#
+
+MACRO(CLONE_LIBRARY LIBNAME VARDIR LIBURI )
+  if ( NOT "${VARDIR}" )
+    if ( HAVE_INCLUDE_${LIBNAME} )
+      set(${VARDIR} "${HAVE_INCLUDE_${LIBNAME}}")
+    else()
+      if ( NOT EXISTS "${CMAKE_SOURCE_DIR}/build/${LIBNAME}")
+        set(store_BUILD_TESTING ${BUILD_TESTING})
+        unset(BUILD_TESTING)
+        execute_process(COMMAND git clone "${LIBURI}" WORKING_DIRECTORY "${CMAKE_SOURCE_DIR}/build")
+        execute_process(COMMAND cmake . WORKING_DIRECTORY "${CMAKE_SOURCE_DIR}/build/${LIBNAME}")
+        execute_process(COMMAND make WORKING_DIRECTORY "${CMAKE_SOURCE_DIR}/build/${LIBNAME}")
+        set(BUILD_TESTING ${store_BUILD_TESTING})
+      endif()
+      set(${VARDIR} "${CMAKE_SOURCE_DIR}/build/${LIBNAME}")
+    endif()
+  endif( NOT "${VARDIR}" )
+ENDMACRO(CLONE_LIBRARY)
+
+MACRO(get_faslib)
+  unset(FASLIB_DIR CACHE)
+  find_path( 
+    FASLIB_DIR NAMES "fas/aop.hpp"
+    PATHS "${CMAKE_CURRENT_SOURCE_DIR}" "${CMAKE_SOURCE_DIR}" 
+    PATH_SUFFIXES "../faslib" "faslib" "build/faslib" "../build/faslib" 
+  )
+  if ( "${FASLIB_DIR}" STREQUAL "FASLIB_DIR-NOTFOUND") 
+    unset(FASLIB_DIR CACHE)
+    clone_library(faslib "FASLIB_DIR" "https://github.com/migashko/faslib.git")
+  endif()
+  include_directories("${FASLIB_DIR}")
+  set(FAS_TESTING_CPP "${FASLIB_DIR}/fas/testing/testing.cpp")
+ENDMACRO(get_faslib)
+
+MACRO(get_mambaru LIBNAME LIBDIR LIBBIN)
+  unset(${${LIBDIR}} CACHE)
+  find_path( 
+    ${LIBDIR} NAMES "${LIBNAME}/${LIBNAME}.hpp"
+    PATHS "${CMAKE_CURRENT_SOURCE_DIR}" "${CMAKE_SOURCE_DIR}" 
+    PATH_SUFFIXES "build/${LIBNAME}" "../build/${LIBNAME}" "${LIBNAME}" "../${LIBNAME}"
+  )
+  if ( "${${LIBDIR}}" STREQUAL "${LIBDIR}-NOTFOUND") 
+    unset(${LIBDIR} CACHE)
+    #clone_library(${LIBNAME} ${LIBDIR} "https://github.com/mambaru/${LIBNAME}.git")
+    clone_library(${LIBNAME} ${LIBDIR} "git@github.lan:cpp/${LIBNAME}.git")
+  endif()
+  MESSAGE(STATUS "${LIBDIR} = ${${LIBDIR}}")
+  include_directories("${${LIBDIR}}")
+  
+  unset(${${LIBBIN}} CACHE)
+  find_library( 
+    ${LIBBIN} NAMES "${LIBNAME}"
+    PATHS "${CMAKE_CURRENT_SOURCE_DIR}" "${CMAKE_SOURCE_DIR}" 
+    PATH_SUFFIXES "build/${LIBNAME}" "../build/${LIBNAME}" "${LIBNAME}" "../${LIBNAME}" "${LIBNAME}/build" "../${LIBNAME}/build"
+  )
+  if ( ${${LIBBIN}} )
+    link_directories("${${LIBBIN}}")
+    MESSAGE(STATUS "${LIBBIN} = ${${LIBBIN}}")
+  endif()
+ENDMACRO(get_mambaru)
+
--- cmake/mambaopt.cmake
+++ cmake/mambaopt.cmake
@@ -1,7 +1,6 @@
-if ( NOT "${CMAKE_CURRENT_SOURCE_DIR}" STREQUAL "${CMAKE_SOURCE_DIR}" )
-  message(STATUS "${PROJECT_NAME} is not top level project")
-  return()
-endif()
+
+set(CMAKE_LIBRARY_OUTPUT_DIRECTORY ${CMAKE_BINARY_DIR})
+set(CMAKE_ARCHIVE_OUTPUT_DIRECTORY ${CMAKE_BINARY_DIR})
 
 if (NOT CMAKE_CXX_STANDARD)
   set(CMAKE_CXX_STANDARD 11)
@@ -24,7 +23,7 @@ if ( ("${CMAKE_CXX_COMPILER_ID}" STREQUAL "Clang")
   set(CMAKE_CXX_FLAGS_DEBUG  "-O0 -g")
   
   if ( "${CMAKE_CXX_COMPILER_ID}" STREQUAL "Clang" )
-    set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -D__STRICT_ANSI__ ") #-stdlib=libc++ 
+    set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -D__STRICT_ANSI__ ${CLANG_LIBPP} ")  # -stdlib=libc++
   endif()
 
   if ( PARANOID_WARNING )
@@ -39,11 +38,11 @@ if ( ("${CMAKE_CXX_COMPILER_ID}" STREQUAL "Clang")
     set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -Wunreachable-code -Wunused -Wunused-function -Wunused-label -Wunused-parameter -Wunused-value")
     set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -Wunused-variable  -Wvariadic-macros -Wvolatile-register-var  -Wwrite-strings")
     set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -Wmissing-include-dirs -Wold-style-cast -Woverloaded-virtual")
-    set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -Wredundant-decls -Wshadow -Wsign-conversion -Wsign-promo")
+    set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -Wredundant-decls -Wshadow -Wsign-conversion -Wsign-promo ")
     set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -Wstrict-overflow=2 -Wswitch -Wswitch-default -Wundef -Werror")
     if ("${CMAKE_CXX_COMPILER_ID}" STREQUAL "GNU")
-      # -Wunsafe-loop-optimizations
       set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -Wlogical-op  -Wnoexcept -Wstrict-null-sentinel -Wno-pragma-once-outside-header")
+      #-Wredundant-move
     endif()
   endif(PARANOID_WARNING)
 elseif ("${CMAKE_CXX_COMPILER_ID}" STREQUAL "Intel")
@@ -54,18 +53,4 @@ elseif ("${CMAKE_CXX_COMPILER_ID}" STREQUAL "MSVC")
   set(CMAKE_CXX_FLAGS_DEBUG  "/Od /D_DEBUG")
   set(CMAKE_CXX_FLAGS_RELWITHDEBINFO  "/Yd /O2 /DNDEBUG")
 endif()
-
-include(ConfigureLibrary)
-CONFIGURE_LIBRARY( fas/aop.hpp "/usr/include/faslib /usr/local/include/faslib" 
-                  faslib "" )
-
-set(store_BUILD_TESTING ${BUILD_TESTING})
-set(BUILD_TESTING OFF)
-clone_library(faslib "FASLIB_DIR" "https://github.com/migashko/faslib.git")
-set(BUILD_TESTING ${store_BUILD_TESTING})
-
-set(FAS_TESTING_CPP "${FASLIB_DIR}/fas/testing/testing.cpp")
-
-include_directories(${FASLIB_DIR})
-include_directories(${CMAKE_CURRENT_SOURCE_DIR})
 
