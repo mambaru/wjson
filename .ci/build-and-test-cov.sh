6a64f0b7fe0e1a0c674c772afee3b51d93240083
Vladimir Migashko <migashko@gmail.com>
2018-10-23 18:28:48 +0300

    up

--- .ci/build-and-test-cov.sh
+++ .ci/build-and-test-cov.sh
@@ -6,4 +6,4 @@ rm -rf *
 cmake -DCMAKE_CXX_COMPILER=$1 -DCMAKE_CXX_STANDARD=$2 -DCMAKE_BUILD_TYPE=Debug -DBUILD_TESTING=ON -DBUILD_COVERAGE=ON "${@:3}" .. || exit $?
 cmake --build . -- -j8 || exit $?
 ctest --output-on-failure
-
+coveralls -r tests/CMakeFiles/test_json.dir/ --gcov-options '\-lpr'
--- .ci/coveralls-show.sh
+++ .ci/coveralls-show.sh
@@ -5,6 +5,6 @@ prj_dir=$(dirname $script_dir)
 prj=$(basename $prj_dir)
 
 rm -r $prj_dir/build/report
-lcov --no-external -d "$prj" -o $prj_dir/build/$prj.info -c -d $prj_dir/build/ 
+lcov --no-external -t "$prj" -o $prj_dir/build/$prj.info -c -d $prj_dir/build/ 
 genhtml -o $prj_dir/build/report $prj_dir/build/$prj.info
 google-chrome $prj_dir/build/report/index.html
