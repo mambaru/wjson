7c96b2939a824b52e3d66039b8e48b344af96a46
Vladimir Migashko <migashko@gmail.com>
2018-11-13 01:57:21 +0300

    cmake-ci upgrade autocommit

--- .ci/build-and-test.sh
+++ .ci/build-and-test.sh
@@ -1,1 +1,9 @@
-../external/cmake-ci/ci/build-and-test.sh
+#!/bin/bash
+date
+mkdir -p build
+cd build || exit $?
+rm -rf *
+cmake -DCMAKE_CXX_COMPILER=$1 -DCMAKE_CXX_STANDARD=$2 -DCMAKE_BUILD_TYPE=$3 -DBUILD_TESTING=ON -DPARANOID_WARNING=ON "${@:3}" .. || exit $?
+cmake --build . -- -j8 || exit $?
+ctest --output-on-failure
+make clean
