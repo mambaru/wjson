2ef240c749c5e2f7b31339a04360a26cb9fd4a53
Vladimir Migashko <migashko@gmail.com>
2019-02-28 17:55:13 +0300

    enum constant in boolean context fix

--- wjson/serializer/flags.hpp
+++ wjson/serializer/flags.hpp
@@ -62,7 +62,7 @@ private:
   template<typename LL, typename RR, typename P>
   static P serialize( const T& v, fas::type_list<LL, RR>, P end, bool isFirst)
   {
-    if ( ( static_cast<bool>(LL::value) && (LL::value & v) == LL::value) || (v == LL::value) )
+    if ( ( (LL::value!=static_cast<T>(0)) && (LL::value & v) == LL::value) || (v == LL::value) )
     {
       if (!isFirst) 
         *(end++) = Sep;
