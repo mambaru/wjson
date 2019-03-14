9ee2cf3f85847fe72d9494dbe3540c99c120fbe7
Vladimir Migashko <migashko@gmail.com>
2019-03-14 13:45:33 +0300

    cmake-ci upgrade autocommit

--- .ci/build-and-deploy.sh
+++ .ci/build-and-deploy.sh
@@ -4,6 +4,7 @@ prjname=$(basename $PWD)
 refname=$(git rev-parse --abbrev-ref HEAD)
 buildtype=$1
 shared_static=$2
+prefix=$3
 shared="ON"
 
 if [[ "$(python3 ./.ci/wamba-ci.py has deploy.$buildtype )" == "OFF" ]]; then
@@ -34,9 +35,9 @@ build_threads="$(python3 ./.ci/wamba-ci.py get build_threads 4)"
 mkdir -p build
 pushd build
   #rm -rf *
-  echo cmake .. -DCMAKE_BUILD_TYPE=$buildtype -DBUILD_SHARED_LIBS=$shared -DCMAKE_CXX_STANDARD=$standard "${@:3}" || exit $?
+  echo cmake .. -DCMAKE_BUILD_TYPE=$buildtype -DBUILD_SHARED_LIBS=$shared -DCMAKE_CXX_STANDARD=$standard "${@:4}" || exit $?
   cmake .. -DCMAKE_BUILD_TYPE=$buildtype -DBUILD_SHARED_LIBS=$shared \
-           -DCMAKE_CXX_STANDARD=$standard -DCMAKE_CXX_COMPILER=$compiler "${@:3}" || exit $?
+           -DCMAKE_CXX_STANDARD=$standard -DCMAKE_CXX_COMPILER=$compiler "${@:4}" || exit $?
   cmake --build . -- -j$build_threads || exit $?
 popd
 
@@ -53,7 +54,7 @@ function deploy()
   cp -rfH ./configurations/common/* ./build/deploy/lead-up/ 2>/dev/null || true
   cp -rfH ./configurations/$1/* ./build/deploy/lead-up/ 2>/dev/null || true
   cp ./.ci/start.sh "./build/deploy/lead-up/${prjname}.sh" 2>/dev/null || true
-  arname="$prjname-$refname-$compiler$standard-$shared_static-$buildtype-$1.tar.gz"
+  arname="$prefix$prjname-$refname-$compiler$standard-$shared_static-$buildtype-$1.tar.gz"
   escaped_arname=$(printf '%s\n' "$arname" | sed 's:[\/&]:\\&:g;$!s/$/\\/')
   cat ./.ci/deploy.sh | sed -e 's/replaceme/${escaped_arname}/g' > ./build/deploy/lead-up/deploy.sh
   chmod +x ./build/deploy/lead-up/deploy.sh
