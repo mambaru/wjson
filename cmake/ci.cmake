cdec7937978a6c6cede7c2d08f7d4baa0cd7dc2b
Vladimir Migashko <migashko@gmail.com>
2019-03-27 23:57:57 +0300

    cmake-ci upgrade autocommit

--- cmake/ci.cmake
+++ cmake/ci.cmake
@@ -6,18 +6,18 @@ endif()
 
 if ( STANDALONE )
 
-  if(NOT EXISTS ${CMAKE_SOURCE_DIR}/external/cmake-ci/cmake/ci.cmake)
+  if(NOT EXISTS ${CMAKE_CURRENT_SOURCE_DIR}/external/cmake-ci/cmake/ci.cmake)
     execute_process(
       COMMAND 
         git submodule update --init -- "external/cmake-ci"
       WORKING_DIRECTORY 
-        ${CMAKE_SOURCE_DIR}
+        ${CMAKE_CURRENT_SOURCE_DIR}
       RESULT_VARIABLE
         EXIT_CODE
     )
   endif()
-  list(APPEND CMAKE_MODULE_PATH ${CMAKE_SOURCE_DIR}/cmake)
-  list(APPEND CMAKE_MODULE_PATH ${CMAKE_SOURCE_DIR}/external/cmake-ci/cmake)
+  list(APPEND CMAKE_MODULE_PATH ${CMAKE_CURRENT_SOURCE_DIR}/cmake)
+  list(APPEND CMAKE_MODULE_PATH ${CMAKE_CURRENT_SOURCE_DIR}/external/cmake-ci/cmake)
   include(opt)
   include(repos)
   include(getlibs)
