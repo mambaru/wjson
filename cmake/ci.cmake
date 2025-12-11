ba71ced92f1a3945276d9e9fe9bffb3542794304
GitLab Runner <gitlab-runner@repobuild4.lan>
2025-12-12 00:13:30 +0300

    Generated export autocommit from mamba.ru

--- cmake/ci.cmake
+++ cmake/ci.cmake
@@ -13,7 +13,7 @@ if ( NOT IS_WCI_PROJECT )
         EXIT_CODE
       ERROR_QUIET
     )
-    
+
     if ( ${EXIT_CODE} EQUAL 0 )
       set(IS_WCI_PROJECT TRUE)
     else()
@@ -22,14 +22,14 @@ if ( NOT IS_WCI_PROJECT )
   else()
     set(IS_WCI_PROJECT TRUE)
   endif()
-  
+
   if ( IS_WCI_PROJECT )
     list(APPEND CMAKE_MODULE_PATH ${path}/external/cmake-ci/cmake)
-    list(APPEND CMAKE_MODULE_PATH ${path}/external/cmake-ci/goodies)
+    list(APPEND CMAKE_MODULE_PATH ${path}/external/cmake-ci/goodies/cmake)
     list(APPEND CMAKE_MODULE_PATH ${path}/cmake)
     include(cmake-ci)
     set( WCI_SUPERMODULE ON)
     set( WCI_DIR "${path}")
   endif()
-  
+
 endif(NOT IS_WCI_PROJECT)
